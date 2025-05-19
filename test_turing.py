cba1518d1acd5f5b88cdc6dd6a695efaf64256a2
Anirudh Yamunan <yamunan.bitset@gmail.com>
2025-05-19 16:23:02 +0200

    unittest for number of correct moves

--- test_turing.py
+++ test_turing.py
@@ -19,7 +19,8 @@ class TestTuringMachine(unittest.TestCase):
 
         x = self.tm.next_move()
         self.assertEqual(x, "HLT!! Turing machine halted.")
-
+        self.assertEqual(self.tm.number_of_moves, 3)
+        self.assertEqual(self.tm.done, True)
 
     def test_2states_2symbols(self):
         # Test the Turing machine with 2 states and 2 symbols
@@ -33,6 +34,8 @@ class TestTuringMachine(unittest.TestCase):
             self.tm.next_move()
         x = self.tm.next_move()
         self.assertEqual(x, "HLT!! Turing machine halted.")
+        self.assertEqual(self.tm.number_of_moves, 6)
+        self.assertEqual(self.tm.done, True)
 
     def test_busy_beaver_4(self):
         #self.rules = [['1R1', '1L1'], ['1L2', '0L0'], ['1R3', '1L0'], ['1R0', 'HLT']] # Should halt after 107 steps
@@ -48,7 +51,8 @@ class TestTuringMachine(unittest.TestCase):
             self.tm.next_move()
         x = self.tm.next_move()
         self.assertEqual(x, "HLT!! Turing machine halted.")
-        
+        self.assertEqual(self.tm.number_of_moves, 107)
+        self.assertEqual(self.tm.done, True)
 
 if __name__ == '__main__':
     unittest.main()
